cc0e0f07bafc4ac76c23ab55656805608a069649
Amrutha-git <varshinibhat30@gmail.com>
2020-04-20 11:14:32 +0530

    first commit

---
+++ backend/middleware/logger.js
@@ -0,0 +1,10 @@
+//Middlware functions
+const logger = (req, res, next) => {
+    console.log(
+      `${req.method} ${req.protocol}://${req.get("host")}${req.originalUrl}`
+    );
+    next();
+  };
+  
+  module.exports = logger;
+  
